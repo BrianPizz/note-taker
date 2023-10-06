9bc6d805eece65a931d3761c697ce3e99f4f0791
BrianPizz <bpizzimentijr@outlook.com>
2023-10-05 21:00:12 -0700

    linked notes page and setup post request

--- server.js
+++ server.js
@@ -14,6 +14,27 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.post('/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        };
+
+        const noteString = JSON.stringify(newNote);
+
+        
+    }
+})
+
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
